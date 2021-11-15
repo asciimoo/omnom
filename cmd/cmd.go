b3036c5170e624539088fefc1ddc80c30322e0c4
Adam Tauber <asciimoo@gmail.com>
2021-11-15 23:37:38 +0100

    [enh] add create-user cmd

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"log"
+
 	"github.com/asciimoo/omnom/config"
 	"github.com/asciimoo/omnom/model"
 	"github.com/asciimoo/omnom/webapp"
@@ -11,6 +13,13 @@ import (
 var cfgFile string
 var cfg *config.Config
 
+func initDB(cmd *cobra.Command, args []string) {
+	err := model.Init(cfg)
+	if err != nil {
+		panic(err)
+	}
+}
+
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "omnom",
@@ -19,11 +28,28 @@ var rootCmd = &cobra.Command{
 }
 
 var listenCmd = &cobra.Command{
-	Use:   "listen",
-	Short: "Start server",
-	Long:  ``,
+	Use:    "listen",
+	Short:  "Start server",
+	Long:   ``,
+	PreRun: initDB,
 	Run: func(cmd *cobra.Command, args []string) {
 		webapp.Run(cfg)
+	},
+}
+
+var createUserCmd = &cobra.Command{
+	Use:    "create-user",
+	Short:  "Create new user",
+	Long:   `create-user [username] [email]`,
+	Args:   cobra.ExactArgs(2),
+	PreRun: initDB,
+	Run: func(cmd *cobra.Command, args []string) {
+		err := model.CreateUser(args[0], args[1])
+		if err != nil {
+			log.Println("Cannot create new user:", err)
+		} else {
+			log.Println("User", args[0], "successfully created")
+		}
 	},
 }
 
@@ -40,6 +66,7 @@ func init() {
 
 	rootCmd.PersistentFlags().BoolP("debug", "d", false, "Turn on debug mode")
 	rootCmd.AddCommand(listenCmd)
+	rootCmd.AddCommand(createUserCmd)
 }
 
 func initConfig() {
@@ -48,10 +75,6 @@ func initConfig() {
 	if b, _ := rootCmd.PersistentFlags().GetBool("debug"); b {
 		cfg.App.Debug = true
 	}
-	if err != nil {
-		panic(err)
-	}
-	err = model.Init(cfg)
 	if err != nil {
 		panic(err)
 	}
