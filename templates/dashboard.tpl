89dfd3ff3ebe39917ac080017c7f0bf0a2ad0226
Adam Tauber <asciimoo@gmail.com>
2021-12-02 14:31:13 +0100

    [enh] display popular tags on dashboard

--- templates/dashboard.tpl
+++ templates/dashboard.tpl
@@ -21,6 +21,21 @@
             </div>
         </div>
     </nav>
+    {{ if .Tags }}
+    <h4 class="title">My frequent tags</h4>
+    <div class="field is-grouped is-grouped-multiline">
+        {{ range .Tags }}
+        <div class="control">
+            <div class="tags has-addons">
+                <span class="tag is-primary is-medium">{{ .Tag }}</span>
+                <span class="tag is-medium">{{ .Count }}</span>
+            </div>
+        </div>
+        {{ end }}
+    </div>
+    {{ else }}
+    {{ block "info" "Add tags to your bookmarks to be able to efficiently filter" }}{{ end }}
+    {{ end }}
     {{ if .Bookmarks }}
       <h4 class="title">My latest bookmarks</h4>
       {{ range .Bookmarks }}
--- webapp/dashboard.go
+++ webapp/dashboard.go
@@ -9,21 +9,29 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type TagCount struct {
+	Tag   string
+	Count int64
+}
+
 func dashboard(c *gin.Context, u *model.User) {
 	var weeklyBookmarkCount int64
 	var monthlyBookmarkCount int64
 	var yearlyBookmarkCount int64
 	var bs []*model.Bookmark
 	now := time.Now()
+	var tags []*TagCount
 	today := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, now.Location())
 	model.DB.Model(&model.Bookmark{}).Where("bookmarks.user_id = ? and bookmarks.created_at > ? and bookmarks.created_at < ?", u.ID, today.Truncate(time.Hour*7*24), now).Count(&weeklyBookmarkCount)
 	model.DB.Model(&model.Bookmark{}).Where("bookmarks.user_id = ? and bookmarks.created_at > ? and bookmarks.created_at < ?", u.ID, today.AddDate(0, -1, 0), now).Count(&monthlyBookmarkCount)
 	model.DB.Model(&model.Bookmark{}).Where("bookmarks.user_id = ? and bookmarks.created_at > ? and bookmarks.created_at < ?", u.ID, today.AddDate(-1, 0, 0), now).Count(&yearlyBookmarkCount)
 	model.DB.Limit(10).Model(u).Preload("Snapshots").Preload("Tags").Order("created_at desc").Association("Bookmarks").Find(&bs)
+	model.DB.Limit(20).Table("tags").Select("tags.text as tag, count(bookmarks.user_id) as `count`").Joins("join bookmarks on bookmarks.id == tags.bookmark_id").Where("bookmarks.user_id = ?", u.ID).Group("tags.text").Order("`count` desc").Find(&tags)
 	renderHTML(c, http.StatusOK, "dashboard", map[string]interface{}{
 		"WeeklyBookmarkCount":  weeklyBookmarkCount,
 		"MonthlyBookmarkCount": monthlyBookmarkCount,
 		"YearlyBookmarkCount":  yearlyBookmarkCount,
 		"Bookmarks":            bs,
+		"Tags":                 tags,
 	})
 }
