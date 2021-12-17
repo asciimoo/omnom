366a4d7fe91fcd4cd4b2d5ab36ba0140391556a0
DavidFucsko <42489885+DavidFucsko@users.noreply.github.com>
2021-12-17 14:54:47 +0100

    [fix] options page error

--- static/ext/src/js/modules/options-page.js
+++ static/ext/src/js/modules/options-page.js
@@ -9,16 +9,32 @@ export default function () {
         window.close();
     }
 
-    function restoreOptions() {
-        chrome.storage.local.get(['omnom_url', 'omnom_token', 'omnom_debug'], function (data) {
-            document.querySelector('#url').value = data.omnom_url || '';
-            document.querySelector('#token').value = data.omnom_token || '';
-            document.querySelector('#debug').checked = data.omnom_debug;
-        });
+    function loadContent() {
+        const template = document.getElementById('options-body');
+        const container = document.createElement('div');
+        container.className = 'omnom-popup__container options-page__container';
+
+        const optionsContent = [...template.content.children];
+        optionsContent.forEach(child => container.appendChild(child));
+        document.body.appendChild(container);
+        restoreOptions();
+        document.querySelector('form').addEventListener('submit', saveOptions);
     }
 
-    document.addEventListener('DOMContentLoaded', restoreOptions);
-    document.querySelector('form').addEventListener('submit', saveOptions);
+    function restoreOptions() {
+        const optionsElement = document.getElementById('omnom-options');
+        if (optionsElement) {
+            chrome.storage.local.get(['omnom_url', 'omnom_token', 'omnom_debug'], function (data) {
+                document.querySelector('#url').value = data.omnom_url || '';
+                document.querySelector('#token').value = data.omnom_token || '';
+                document.querySelector('#debug').checked = data.omnom_debug;
+            });
+        } else {
+            document.addEventListener('DOMContentLoaded', () => {
+                loadContent();
+            })
+        }
+    }
     (function onLoad() {
         restoreOptions();
     })()
