6c8c7ee7c23718f0a4aaa6fbdc6c45846e334c81
Adam Tauber <asciimoo@gmail.com>
2022-02-07 15:37:47 +0100

    [enh] add arguments to api endpoints

--- webapp/api.go
+++ webapp/api.go
@@ -60,6 +60,20 @@ func init() {
 			AuthRequired: false,
 			Handler:      signup,
 			Description:  "Signup form processor",
+			Args: []*EndpointArg{
+				&EndpointArg{
+					Name:        "username",
+					Type:        "string",
+					Required:    true,
+					Description: "Username of the new account",
+				},
+				&EndpointArg{
+					Name:        "email",
+					Type:        "string",
+					Required:    true,
+					Description: "Email address of the new account",
+				},
+			},
 		},
 		&Endpoint{
 			Name:         "Login form",
@@ -76,6 +90,14 @@ func init() {
 			AuthRequired: false,
 			Handler:      login,
 			Description:  "Login form processor",
+			Args: []*EndpointArg{
+				&EndpointArg{
+					Name:        "username",
+					Type:        "string",
+					Required:    true,
+					Description: "Username or email address of the new account",
+				},
+			},
 		},
 		&Endpoint{
 			Name:         "Logout page",
@@ -92,6 +114,56 @@ func init() {
 			AuthRequired: false,
 			Handler:      bookmarks,
 			Description:  "Displays public bookmarks with optional filters",
+			Args: []*EndpointArg{
+				&EndpointArg{
+					Name:        "query",
+					Type:        "string",
+					Required:    false,
+					Description: "Search term to filter bookmarks by title",
+				},
+				&EndpointArg{
+					Name:        "owner",
+					Type:        "string",
+					Required:    false,
+					Description: "Search term to filter bookmarks by username",
+				},
+				&EndpointArg{
+					Name:        "tag",
+					Type:        "string",
+					Required:    false,
+					Description: "Search term to filter bookmarks by tag",
+				},
+				&EndpointArg{
+					Name:        "domain",
+					Type:        "string",
+					Required:    false,
+					Description: "Search term to filter bookmarks by domain",
+				},
+				&EndpointArg{
+					Name:        "from",
+					Type:        "date",
+					Required:    false,
+					Description: "Display only newer bookmarks. (Format: YYYY.MM.DD)",
+				},
+				&EndpointArg{
+					Name:        "to",
+					Type:        "date",
+					Required:    false,
+					Description: "Display only older bookmarks. (Format: YYYY.MM.DD)",
+				},
+				&EndpointArg{
+					Name:        "search_in_snapshots",
+					Type:        "boolean",
+					Required:    false,
+					Description: "Query parameter also applied to snapshot content. (Values: 0, 1)",
+				},
+				&EndpointArg{
+					Name:        "search_in_notes",
+					Type:        "boolean",
+					Required:    false,
+					Description: "Query parameter also applied to bookmark notes. (Values: 0, 1)",
+				},
+			},
 		},
 		&Endpoint{
 			Name:         "Snapshot view with details",
@@ -100,6 +172,20 @@ func init() {
 			AuthRequired: false,
 			Handler:      snapshotWrapper,
 			Description:  "Displays bookmark snapshots with additional bookmark properties",
+			Args: []*EndpointArg{
+				&EndpointArg{
+					Name:        "sid",
+					Type:        "string",
+					Required:    true,
+					Description: "Snapshot key",
+				},
+				&EndpointArg{
+					Name:        "bid",
+					Type:        "int",
+					Required:    true,
+					Description: "Bookmark ID",
+				},
+			},
 		},
 		&Endpoint{
 			Name:         "Fullscreen snapshot view",
@@ -108,6 +194,14 @@ func init() {
 			AuthRequired: false,
 			Handler:      snapshot,
 			Description:  "Displays bookmark snapshots as a fullscreen page",
+			Args: []*EndpointArg{
+				&EndpointArg{
+					Name:        "sid",
+					Type:        "string",
+					Required:    true,
+					Description: "Snapshot key",
+				},
+			},
 		},
 		&Endpoint{
 			Name:         "Add bookmark",
@@ -116,6 +210,68 @@ func init() {
 			AuthRequired: false,
 			Handler:      addBookmark,
 			Description:  "Add new bookmark",
+			Args: []*EndpointArg{
+				&EndpointArg{
+					Name:        "token",
+					Type:        "string",
+					Required:    true,
+					Description: "Extension token. It can be found on the profile page",
+				},
+				&EndpointArg{
+					Name:        "url",
+					Type:        "URL",
+					Required:    true,
+					Description: "URL of the bookmark",
+				},
+				&EndpointArg{
+					Name:        "title",
+					Type:        "string",
+					Required:    true,
+					Description: "Title of the bookmark",
+				},
+				&EndpointArg{
+					Name:        "notes",
+					Type:        "string",
+					Required:    false,
+					Description: "Bookmark notes",
+				},
+				&EndpointArg{
+					Name:        "favicon",
+					Type:        "string",
+					Required:    false,
+					Description: "Data URL encoded favicon (https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URIs)",
+				},
+				&EndpointArg{
+					Name:        "public",
+					Type:        "boolean",
+					Required:    false,
+					Description: "Marks bookmark as public",
+				},
+				&EndpointArg{
+					Name:        "tags",
+					Type:        "string",
+					Required:    false,
+					Description: "Comma separated list of tags",
+				},
+				&EndpointArg{
+					Name:        "snapshot_title",
+					Type:        "string",
+					Required:    false,
+					Description: "Title of the snapshot",
+				},
+				&EndpointArg{
+					Name:        "snapshot_text",
+					Type:        "string",
+					Required:    false,
+					Description: "Text content of the snapshot",
+				},
+				&EndpointArg{
+					Name:        "snapshot",
+					Type:        "multipart file",
+					Required:    false,
+					Description: "Snapshot file",
+				},
+			},
 		},
 		&Endpoint{
 			Name:         "Check bookmark",
@@ -124,6 +280,20 @@ func init() {
 			AuthRequired: false,
 			Handler:      checkBookmark,
 			Description:  "Checks if a bookmark is already exists",
+			Args: []*EndpointArg{
+				&EndpointArg{
+					Name:        "token",
+					Type:        "string",
+					Required:    true,
+					Description: "Extension token. It can be found on the profile page",
+				},
+				&EndpointArg{
+					Name:        "url",
+					Type:        "URL",
+					Required:    true,
+					Description: "URL of the bookmark",
+				},
+			},
 		},
 		&Endpoint{
 			Name:         "View bookmark",
@@ -132,6 +302,14 @@ func init() {
 			AuthRequired: false,
 			Handler:      viewBookmark,
 			Description:  "Displays all details of a bookmark",
+			Args: []*EndpointArg{
+				&EndpointArg{
+					Name:        "id",
+					Type:        "int",
+					Required:    true,
+					Description: "Bookmark ID",
+				},
+			},
 		},
 		&Endpoint{
 			Name:         "API documentation",
@@ -141,9 +319,9 @@ func init() {
 			Handler:      api,
 			Description:  "Displays API documentation (this page)",
 		},
-		/*\
-		|*| LOGIN REQUIRED FOR THE ENDPOINTS BELOW
-		\*/
+		/****************************************\
+		| LOGIN REQUIRED FOR THE ENDPOINTS BELOW |
+		\****************************************/
 		&Endpoint{
 			Name:         "Profile page",
 			Path:         "/profile",
@@ -167,6 +345,14 @@ func init() {
 			AuthRequired: true,
 			Handler:      deleteAddonToken,
 			Description:  "Deletes an addon token",
+			Args: []*EndpointArg{
+				&EndpointArg{
+					Name:        "id",
+					Type:        "int",
+					Required:    true,
+					Description: "Token ID",
+				},
+			},
 		},
 		&Endpoint{
 			Name:         "View personal bookmarks",
@@ -175,6 +361,62 @@ func init() {
 			AuthRequired: true,
 			Handler:      myBookmarks,
 			Description:  "Displays bookmarks belongs to the current user with optional filters",
+			Args: []*EndpointArg{
+				&EndpointArg{
+					Name:        "query",
+					Type:        "string",
+					Required:    false,
+					Description: "Search term to filter bookmarks by title",
+				},
+				&EndpointArg{
+					Name:        "tag",
+					Type:        "string",
+					Required:    false,
+					Description: "Search term to filter bookmarks by tag",
+				},
+				&EndpointArg{
+					Name:        "domain",
+					Type:        "string",
+					Required:    false,
+					Description: "Search term to filter bookmarks by domain",
+				},
+				&EndpointArg{
+					Name:        "from",
+					Type:        "date",
+					Required:    false,
+					Description: "Display only newer bookmarks. (Format: YYYY.MM.DD)",
+				},
+				&EndpointArg{
+					Name:        "to",
+					Type:        "date",
+					Required:    false,
+					Description: "Display only older bookmarks. (Format: YYYY.MM.DD)",
+				},
+				&EndpointArg{
+					Name:        "is_public",
+					Type:        "boolean",
+					Required:    false,
+					Description: "Display only public bookmarks. (Values: 0, 1)",
+				},
+				&EndpointArg{
+					Name:        "is_private",
+					Type:        "boolean",
+					Required:    false,
+					Description: "Display only private bookmarks. (Values: 0, 1)",
+				},
+				&EndpointArg{
+					Name:        "search_in_snapshots",
+					Type:        "boolean",
+					Required:    false,
+					Description: "Query parameter also applied to snapshot content. (Values: 0, 1)",
+				},
+				&EndpointArg{
+					Name:        "search_in_notes",
+					Type:        "boolean",
+					Required:    false,
+					Description: "Query parameter also applied to bookmark notes. (Values: 0, 1)",
+				},
+			},
 		},
 		&Endpoint{
 			Name:         "Edit bookmark",
@@ -183,6 +425,14 @@ func init() {
 			AuthRequired: true,
 			Handler:      editBookmark,
 			Description:  "Displays a bookmark with all the editable properties",
+			Args: []*EndpointArg{
+				&EndpointArg{
+					Name:        "id",
+					Type:        "int",
+					Required:    true,
+					Description: "Bookmark ID",
+				},
+			},
 		},
 		&Endpoint{
 			Name:         "Save bookmark",
@@ -191,6 +441,32 @@ func init() {
 			AuthRequired: true,
 			Handler:      saveBookmark,
 			Description:  "Saves an edited bookmark",
+			Args: []*EndpointArg{
+				&EndpointArg{
+					Name:        "id",
+					Type:        "int",
+					Required:    true,
+					Description: "Bookmark ID",
+				},
+				&EndpointArg{
+					Name:        "title",
+					Type:        "string",
+					Required:    true,
+					Description: "Title of the bookmark",
+				},
+				&EndpointArg{
+					Name:        "notes",
+					Type:        "string",
+					Required:    false,
+					Description: "Bookmark notes",
+				},
+				&EndpointArg{
+					Name:        "public",
+					Type:        "boolean",
+					Required:    false,
+					Description: "Bookmark is publicly accessible. (Omit this argument in case of private bookmarks)",
+				},
+			},
 		},
 		&Endpoint{
 			Name:         "Delete snapshot",
@@ -199,6 +475,20 @@ func init() {
 			AuthRequired: true,
 			Handler:      deleteSnapshot,
 			Description:  "Deletes a snapshot",
+			Args: []*EndpointArg{
+				&EndpointArg{
+					Name:        "bid",
+					Type:        "int",
+					Required:    true,
+					Description: "Bookmark ID",
+				},
+				&EndpointArg{
+					Name:        "sid",
+					Type:        "int",
+					Required:    true,
+					Description: "Snapshot ID",
+				},
+			},
 		},
 		&Endpoint{
 			Name:         "Delete bookmark",
@@ -207,6 +497,14 @@ func init() {
 			AuthRequired: true,
 			Handler:      deleteBookmark,
 			Description:  "Deletes a bookmark",
+			Args: []*EndpointArg{
+				&EndpointArg{
+					Name:        "id",
+					Type:        "int",
+					Required:    true,
+					Description: "Bookmark ID",
+				},
+			},
 		},
 		&Endpoint{
 			Name:         "Add tag",
@@ -215,6 +513,20 @@ func init() {
 			AuthRequired: true,
 			Handler:      addTag,
 			Description:  "Add tag to a bookmark",
+			Args: []*EndpointArg{
+				&EndpointArg{
+					Name:        "bid",
+					Type:        "int",
+					Required:    true,
+					Description: "Bookmark ID",
+				},
+				&EndpointArg{
+					Name:        "tag",
+					Type:        "string",
+					Required:    true,
+					Description: "Tag string",
+				},
+			},
 		},
 		&Endpoint{
 			Name:         "Delete tag",
@@ -223,6 +535,20 @@ func init() {
 			AuthRequired: true,
 			Handler:      deleteTag,
 			Description:  "Delete tag's bookmark",
+			Args: []*EndpointArg{
+				&EndpointArg{
+					Name:        "bid",
+					Type:        "int",
+					Required:    true,
+					Description: "Bookmark ID",
+				},
+				&EndpointArg{
+					Name:        "tid",
+					Type:        "int",
+					Required:    true,
+					Description: "Tag ID",
+				},
+			},
 		},
 	}
 }
