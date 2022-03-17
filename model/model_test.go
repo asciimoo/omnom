074ff082255f5e22bf7a69ebf401c21bad91f483
Adam Tauber <asciimoo@gmail.com>
2022-03-17 13:25:39 +0100

    [enh] add sqlite init test

---
+++ model/model_test.go
@@ -0,0 +1,23 @@
+package model
+
+import (
+	"os"
+	"testing"
+
+	"github.com/asciimoo/omnom/config"
+)
+
+func TestSQLiteInit(t *testing.T) {
+	db := "./test_db.sqlite3"
+	defer os.Remove(db)
+	cfg := config.Config{
+		DB: config.DB{
+			Connection: db,
+			Type:       "sqlite",
+		},
+	}
+	err := Init(&cfg)
+	if err != nil {
+		t.Errorf("Failed to initialize SQLite DB: %s", err)
+	}
+}
