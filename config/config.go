b8cb0334d96a2f296e972f9cc6649e1b8d159c58
Adam Tauber <asciimoo@gmail.com>
2022-03-17 13:16:40 +0100

    [mod] use dedicated types for config sections

--- config/config.go
+++ config/config.go
@@ -7,34 +7,44 @@ import (
 )
 
 type Config struct {
-	App struct {
-		Debug            bool  `yaml:"debug"`
-		BookmarksPerPage int64 `yaml:"bookmarks_per_page"`
-		DisableSignup    bool  `yaml:"disable_signup"`
-	} `yaml:"app"`
-	Server struct {
-		Address string `yaml:"address"`
-		BaseURL string `yaml:"base_url"`
-	} `yaml:"server"`
-	DB struct {
-		Connection string `yaml:"connection"`
-		Type       string `yaml:"type"`
-	} `yaml:"db"`
-	Storage struct {
-		Type string `yaml:"type"`
-		Root string `yaml:"root"`
-	} `yaml:"storage"`
-	SMTP struct {
-		Host              string `yaml:"host"`
-		Port              int    `yaml:"port"`
-		Username          string `yaml:"username"`
-		Password          string `yaml:"password"`
-		Sender            string `yaml:"sender"`
-		TLS               bool   `yaml:"tls"`
-		TLSAllowInsecure  bool   `yaml:"tls_allow_insecure"`
-		SendTimeout       int    `yaml:"send_timeout"`
-		ConnectionTimeout int    `yaml:"connection_timeout"`
-	} `yaml:"smtp"`
+	App     App     `yaml:"app"`
+	Server  Server  `yaml:"server"`
+	DB      DB      `yaml:"db"`
+	Storage Storage `yaml:"storage"`
+	SMTP    SMTP    `yaml:"smtp"`
+}
+
+type App struct {
+	Debug            bool  `yaml:"debug"`
+	BookmarksPerPage int64 `yaml:"bookmarks_per_page"`
+	DisableSignup    bool  `yaml:"disable_signup"`
+}
+
+type Server struct {
+	Address string `yaml:"address"`
+	BaseURL string `yaml:"base_url"`
+}
+
+type DB struct {
+	Connection string `yaml:"connection"`
+	Type       string `yaml:"type"`
+}
+
+type Storage struct {
+	Type string `yaml:"type"`
+	Root string `yaml:"root"`
+}
+
+type SMTP struct {
+	Host              string `yaml:"host"`
+	Port              int    `yaml:"port"`
+	Username          string `yaml:"username"`
+	Password          string `yaml:"password"`
+	Sender            string `yaml:"sender"`
+	TLS               bool   `yaml:"tls"`
+	TLSAllowInsecure  bool   `yaml:"tls_allow_insecure"`
+	SendTimeout       int    `yaml:"send_timeout"`
+	ConnectionTimeout int    `yaml:"connection_timeout"`
 }
 
 func Load(filename string) (*Config, error) {
