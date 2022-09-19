d716b4b3db24711c22daa9f1c5de0a02ec6718cf
Adam Tauber <asciimoo@gmail.com>
2022-09-19 17:03:07 +0200

    [enh] add TEST env var

--- ext/utils/env.js
+++ ext/utils/env.js
@@ -1,4 +1,5 @@
 module.exports = {
     NODE_ENV: (process.env.NODE_ENV || "development"),
-    PORT: (process.env.PORT || 3000)
+    PORT: (process.env.PORT || 3000),
+    TEST: process.env.TEST
 };
