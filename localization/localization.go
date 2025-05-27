59b5143632f29a962883b1c10c8a2f000100ad29
Adam Tauber <asciimoo@gmail.com>
2025-05-27 21:18:28 +0200

    [enh] fall back to default language if translation string is not found

--- localization/localization.go
+++ localization/localization.go
@@ -16,6 +16,7 @@ import (
 var fs embed.FS
 var bundle *i18n.Bundle
 var SupportedLanguages []*LangInfo
+var defaultLocalizer *Localizer
 
 type LangInfo struct {
 	Abbr        string
@@ -40,8 +41,8 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
-	SupportedLanguages = make([]*LangInfo, len(d))
-	for i, f := range d {
+	SupportedLanguages = make([]*LangInfo, 0)
+	for _, f := range d {
 		m, err := bundle.LoadMessageFileFS(fs, "locales/"+f.Name())
 		if err != nil {
 			log.Error().Err(err).Str("file", f.Name()).Msg("Failed to load translation file")
@@ -56,34 +57,45 @@ func init() {
 			log.Error().Err(err).Str("lang", lang).Msg("Failed to identify translation language")
 			os.Exit(1)
 		}
-		SupportedLanguages[i] = &LangInfo{
+		SupportedLanguages = append(SupportedLanguages, &LangInfo{
 			Abbr:        lang,
 			EnglishName: display.English.Tags().Name(t),
 			Name:        display.Self.Name(t),
-		}
+		})
 		m.Tag = t
 	}
+	defaultLocalizer = NewLocalizer("en")
 }
 
 func (l *Localizer) Msg(msg string) string {
-	tm, err := l.l.LocalizeMessage(&i18n.Message{
+	m := &i18n.Message{
 		ID: msg,
-	})
+	}
+	tm, err := l.l.LocalizeMessage(m)
 	if err != nil {
-		log.Debug().Err(err).Msg("Missing translation")
-		return msg
+		tem, err := defaultLocalizer.l.LocalizeMessage(m)
+		if err != nil {
+			log.Debug().Err(err).Str("id", msg).Msg("Missing translation")
+			return msg
+		}
+		return tem
 	}
 	return tm
 }
 
 func (l *Localizer) Data(msg string, data map[string]interface{}) string {
-	tm, err := l.l.Localize(&i18n.LocalizeConfig{
+	m := &i18n.LocalizeConfig{
 		MessageID:    msg,
 		TemplateData: data,
-	})
+	}
+	tm, err := l.l.Localize(m)
 	if err != nil {
-		log.Debug().Err(err).Msg("Missing translation")
-		return msg
+		tem, err := defaultLocalizer.l.Localize(m)
+		if err != nil {
+			log.Debug().Err(err).Str("id", msg).Msg("Missing translation")
+			return msg
+		}
+		return tem
 	}
 	return tm
 }
